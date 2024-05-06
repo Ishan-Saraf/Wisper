96aeb61bd3b736f7c69a81388ff9ad87c98ee044
Ishan-Saraf <ishaansaraf03@gmail.com>
2024-05-06 21:46:14 +0530

    Initial commit

---
+++ public/wisper/src/components/Logout.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { BiPowerOff } from "react-icons/bi";
+import styled from "styled-components";
+import axios from "axios";
+import { logoutRoute } from "../utils/APIRoutes";
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  background-color: #9a86f3;
+  border: none;
+  cursor: pointer;
+  svg {
+    font-size: 1.3rem;
+    color: #ebe7ff;
+  }
+`;
+
+const getUserIdFromLocalStorage = () => {
+  const userData = localStorage.getItem("chat-app-user");
+  return userData? JSON.parse(userData)._id : null;
+};
+
+const handleLogout = async (id) => {
+  try {
+    const response = await axios.get(`${logoutRoute}/${id}`);
+    if (response.status === 200) {
+      localStorage.clear();
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
+const Logout = () => {
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+    const userId = getUserIdFromLocalStorage();
+    if (!userId) {
+      console.error("User ID not found in local storage");
+      return;
+    }
+    const logoutSuccess = await handleLogout(userId);
+    if (logoutSuccess) {
+      navigate("/login");
+    }
+  };
+
+  return (
+    <Button onClick={handleClick}>
+      <BiPowerOff />
+    </Button>
+  );
+};
+
+export default Logout;
---
+++ public/wisper/src/pages/Chat.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { io } from "socket.io-client";
+import styled from "styled-components";
+import { allUsersRoute, host } from "../utils/APIRoutes";
+import ChatContainer from "../components/ChatContainer";
+import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
+
+export default function Chat() {
+  const navigate = useNavigate();
+  const socket = useRef();
+  const [contacts, setContacts] = useState([]);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  const authenticateUser = async () => {
+    const userData = localStorage.getItem("chat-app-user");
+    if (!userData) {
+      navigate("/login");
+    } else {
+      const data = await JSON.parse(userData);
+      setCurrentUser(data);
+    }
+  };
+
+  useEffect(() => {
+    authenticateUser();
+  },);
+
+  const connectToSocket = () => {
+    if (currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);
+    }
+  };
+
+  useEffect(() => {
+    connectToSocket();
+  }, [currentUser]);
+
+  const fetchContacts = async () => {
+    if (currentUser && currentUser.isAvatarImageSet) {
+      try {
+        const { data } = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+        setContacts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    } else if (currentUser) {
+      navigate("/setAvatar");
+    }
+  };
+
+  useEffect(() => {
+    fetchContacts();
+  }, [currentUser]);
+
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
+
+  return (
+    <>
+      <Container>
+        <div className="container">
+          <Contacts contacts={contacts} changeChat={handleChatChange} />
+          {currentChat === undefined? (
+            <Welcome />
+          ) : (
+            <ChatContainer currentChat={currentChat} socket={socket} />
+          )}
+        </div>
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+ .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
